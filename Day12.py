46bc2fa35bca64fc102e357661e0cc835f03ad7b
Vanya Verma <72427243+vanya31@users.noreply.github.com>
2022-12-08 21:37:43 +0530

    Strings

---
+++ Day12.py
@@ -0,0 +1,19 @@
+name = "Vanya"
+friend = "Saksham"
+anotherFriend = 'Vani'
+apple = '''He said, 
+Hi Vanya
+hey I am good
+"I want to eat an apple'''
+ 
+print("Hello, " + name)
+# print(apple) 
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+# print(name[5]) # Throws an error
+print("Lets use a for loop\n")
+for character in apple:
+    print(character)
